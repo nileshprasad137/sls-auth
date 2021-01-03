8baed53d30927346ad886c252a0febf40663cf8f
nileshprasad137 <prasadnilesh96@gmail.com>
2021-01-03 16:19:07 +0530

    making sense of everything, its too complicated for first timers

--- sls-auth0-handler/src/handler.py
+++ sls-auth0-handler/src/handler.py
@@ -8,7 +8,7 @@ from src.response import return_success, return_failure
 
 
 class Settings:
-    AUTH0_CALLBACK_URL = os.environ.get("AUTH0_CALLBACK_URL")
+    AUTH0_AUTH_CODE_CALLBACK_URL = os.environ.get("AUTH0_AUTH_CODE_CALLBACK_URL")
     AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
     AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
     AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
@@ -19,15 +19,17 @@ class Auth0:
     conn = http.client.HTTPSConnection(Settings.AUTH0_DOMAIN)
 
     @classmethod
-    def get_token(cls, code: str) -> dict:
-        """Getting tokens
+    def get_access_token(cls, code: str) -> dict:
+        """
+            Getting access token for hitting our service endpoints.
+            We exchange our authorisation code with access token by hitting /oauth/token of Auth Server.
         """
         payload = {
             "grant_type": "authorization_code",
             "client_id": Settings.AUTH0_CLIENT_ID,
             "client_secret": Settings.AUTH0_CLIENT_SECRET,
             "code": code,
-            "redirect_uri": Settings.AUTH0_CALLBACK_URL
+            "redirect_uri": Settings.AUTH0_AUTH_CODE_CALLBACK_URL
         }
 
         headers = {
@@ -47,32 +49,13 @@ class Auth0:
     def get_user_profile(cls, code: str) -> dict:
         """Getting User Profile
         """
-        token = cls.get_token(code)
-        if token.get('id_token') is None or token.get('access_token') is None:
-            return None
-
-        body = json.dumps({
-            'id_token': token.get('id_token')
-        })
-
-        headers = {
-            'Authorization': 'Bearer {}'.format(token.get('access_token')),
-            'Content-Type': 'application/json'
-        }
-
-        cls.conn.request("GET", "/userinfo", body, headers)
-        response = cls.conn.getresponse()
-        data = response.read()
-        results = json.loads(data.decode("utf-8"))
-        print(results)
-        return results
+        token = cls.get_access_token(code)
+        return Auth0.get_user_info(token)
 
     @classmethod
-    def get_user_profile_from_user_info(cls, tokens: dict) -> dict:
-        """Getting User Profile
+    def get_user_info(cls, tokens: dict) -> dict:
+        """ Getting User Profile from /userinfo endpoint. This is OpenID Connect standard.
         """
-        print("token is below")
-        print(tokens)
         if tokens.get('id_token') is None and tokens.get('access_token') is None:
             return None
 
@@ -135,46 +118,32 @@ class Profile:
             status = cls.put_item(item['sub'], item)
             return item if status else None, status
 
-
-def index(event: dict, context):
+def handle_auth_code_login(event: dict, context):
     param = event.get('queryStringParameters', None)
+    """
+        'code' here is authorisation code sent by authorisation server.
+         In our case, authorisation server is Auth0 since we are using it as auth service.
+         However, we  again need to exchange this authorisation code
+         with access_token which is what happens below.
+    """
     if param is None or param.get('code') is None:
-        msg = "Oops, Invalid Request!!!"
+        msg = "Invalid Request. \'code\' is not found in queryStringParameters."
         # TODO: Maybe need to show some permission denied?
         return return_failure({"status": False, "msg": msg})
 
     user_profile = Auth0.get_user_profile(param['code'])
     print(user_profile)
+    # Below, the profile used for login is added to dynamoDB Table. This helps the server-side
+    # to know who logged in
     item, created = Profile.get_or_create(user_profile)
     # TODO: You will need to redirect customers to a specific url here.
     return return_success({
         "status": True, "created?": created, "data": item})
 
 def handle_implicit_login(event: dict, context):
-    # print(json.dumps(event))
-    # param = event.get('queryStringParameters', None)
     body = event.get('body')
     tokens = urllib.parse.parse_qs(body)
     print(tokens)
-    user_profile = Auth0.get_user_profile_from_user_info(tokens)
-
-    # print("body --> " , body)
-    # id_token = body.split("=")[1]
-    # print("id_token ---> ", id_token)
-    # body = json.dumps({
-    #     'id_token': id_token
-    # })
-    #
-    # headers = {
-    #     'Authorization': 'Bearer {}'.format(token.get('access_token')),
-    #     'Content-Type': 'application/json'
-    # }
-    #
-    # cls.conn.request("GET", "/userinfo", body, headers)
-    # response = cls.conn.getresponse()
-    # data = response.read()
-    # results = json.loads(data.decode("utf-8"))
-    # print(results)
-    # user_profile = Auth0.get_user_profile(id_token)
+    user_profile = Auth0.get_user_info(tokens)
     return return_success({
         "status": True, "user_profile": user_profile})
